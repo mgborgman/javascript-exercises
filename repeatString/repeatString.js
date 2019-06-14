652717b8bd96c148138727a244e38e3564e59b40
Matt Borgman <mattborgman@Matts-MacBook-Pro.local>
2019-06-14 16:09:20 -0700

    repeat string exercises and tests

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num == -1) {
+        return 'ERROR'
+    }
+    else if(num == 0) {
+        return ''
+    }
+    else {
+        var result = '';
+        for (var i = 0; i < num; i++) {
+            result += str;
+        }
+        return result;
+    }
 }
 
 module.exports = repeatString
