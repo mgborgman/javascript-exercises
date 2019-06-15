d956b80e9e19c32c1fede23f26e73f29b1c9b814
Matt Borgman <mattborgman@Matts-MacBook-Pro.local>
2019-06-15 10:38:03 -0700

    exercises and tests

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    var start = 0;
+    var stop = 0;
+    var total = 0;
+    if ((typeof(num1) != 'number') ||
+        (typeof(num2) != 'number') || 
+        (num1.isArray) ||
+        (num2.isArray) ||
+        (num1 < 0) ||
+        (num2 < 0)) {
+            return 'ERROR';
+        }
+    else {
+        if(num1 < num2) {
+            start = num1;
+            stop = num2;
+        } else {
+            start = num2;
+            stop = num1;
+        }
 
+        var i = start;
+        for (i; i <= stop; i++) {
+            total += i;
+        }
+    }
+    return total;
 }
 
 module.exports = sumAll
