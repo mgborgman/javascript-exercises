31f1d0592416ab7147c1c99f36d0568e8f1c6bbe
Matt Borgman <mattborgman@Matts-MacBook-Pro.local>
2019-06-14 17:10:20 -0700

    exercises and tests

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(list) {
+    var i, element, index;
+    for (i = 1; i < arguments.length; i++) {
+        element = arguments[i];
+        if(list.includes(element)) {
+            index = list.indexOf(element);
+            list.splice(index, 1);
+        }
+    }
+    return list;
 
 }
 
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var str_arr = str.split('');
+    var result = '';
+    for (var i = str_arr.length - 1; i >= 0; i--) {
+        result += str_arr[i];
+    }
+    return result;
 }
 
 module.exports = reverseString
